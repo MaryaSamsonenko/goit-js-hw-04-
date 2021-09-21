16c241b64f60c71529dc80c20267276400eedd5c
MaryaSamsonenko <84477591+MaryaSamsonenko@users.noreply.github.com>
2021-09-21 23:44:19 +0300

    добавляет решение 40 - 48 задач

--- js/autotest40-48.js
+++ js/autotest40-48.js
@@ -170,14 +170,165 @@
 
 // ============ SOLUTION 45 =============
 
+// const books = [
+//     {
+//         title: 'The Last Kingdom',
+//         author: 'Bernard Cornwell',
+//         rating: 8.38,
+//     },
+//     {
+//         title: 'Beside Still Waters',
+//         author: 'Robert Sheckley',
+//         rating: 8.51,
+//     },
+//     {
+//         title: 'The Dream of a Ridiculous Man',
+//         author: 'Fyodor Dostoevsky',
+//         rating: 7.75,
+//     },
+//     { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//     {
+//         title: 'The Dreams in the Witch House',
+//         author: 'Howard Lovecraft',
+//         rating: 8.67,
+//     },
+// ];
+// const MIN_BOOK_RATING = 8;
+// // Change code below this line
+
+// const names = [...books]
+//     .filter(book => book.rating > MIN_BOOK_RATING)
+//     .sort((a, b) => a.author.localeCompare(b.author))
+//     .map(book => book.author);
+
+// console.log(names);
+
 // ============== TASK 46 ===============
+
+// Дополни функцию getNamesSortedByFriendCount(users) так, чтобы она возвращала массив имён пользователей отсортированный по возрастанию количества их друзей (свойство friends).
+
+// Тесты
+// Объявлена переменная getNamesSortedByFriendCount
+// Переменной getNamesSortedByFriendCount присвоена стрелочная функция с параметром (users)
+// В теле функции используется цепочка методов
+// Значение параметра users не изменяется
+// Вызов функции с указанным массивом пользователей возвращает массив ["Moore Hensley", "Sharlene Bush", "Elma Head", "Sheree Anthony", "Ross Vazquez", "Carey Barr", "Blackburn Dotson"]
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
 
 // ============ SOLUTION 46 =============
 
+// const users = [
+//     {
+//         name: 'Moore Hensley',
+//         email: 'moorehensley@indexia.com',
+//         eyeColor: 'blue',
+//         friends: ['Sharron Pace'],
+//         isActive: false,
+//         balance: 2811,
+//         gender: 'male',
+//     },
+//     {
+//         name: 'Sharlene Bush',
+//         email: 'sharlenebush@tubesys.com',
+//         eyeColor: 'blue',
+//         friends: ['Briana Decker', 'Sharron Pace'],
+//         isActive: true,
+//         balance: 3821,
+//         gender: 'female',
+//     },
+//     {
+//         name: 'Ross Vazquez',
+//         email: 'rossvazquez@xinware.com',
+//         eyeColor: 'green',
+//         friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//         isActive: false,
+//         balance: 3793,
+//         gender: 'male',
+//     },
+//     {
+//         name: 'Elma Head',
+//         email: 'elmahead@omatom.com',
+//         eyeColor: 'green',
+//         friends: ['Goldie Gentry', 'Aisha Tran'],
+//         isActive: true,
+//         balance: 2278,
+//         gender: 'female',
+//     },
+//     {
+//         name: 'Carey Barr',
+//         email: 'careybarr@nurali.com',
+//         eyeColor: 'blue',
+//         friends: ['Jordan Sampson', 'Eddie Strong', 'Adrian Cross'],
+//         isActive: true,
+//         balance: 3951,
+//         gender: 'male',
+//     },
+//     {
+//         name: 'Blackburn Dotson',
+//         email: 'blackburndotson@furnigeer.com',
+//         eyeColor: 'brown',
+//         friends: ['Jacklyn Lucas', 'Linda Chapman', 'Adrian Cross', 'Solomon Fokes'],
+//         isActive: false,
+//         balance: 1498,
+//         gender: 'male',
+//     },
+//     {
+//         name: 'Sheree Anthony',
+//         email: 'shereeanthony@kog.com',
+//         eyeColor: 'brown',
+//         friends: ['Goldie Gentry', 'Briana Decker'],
+//         isActive: true,
+//         balance: 2764,
+//         gender: 'female',
+//     },
+// ];
+
+// const getNamesSortedByFriendCount = users => {
+//     return [...users]
+//         .sort((firstUser, secondUser) => firstUser.friends.length - secondUser.friends.length)
+//         .map(user => user.name);
+// };
+
+// console.log(getNamesSortedByFriendCount(users));
+
 // ============== TASK 47 ===============
+
+// Дополни функцию getSortedFriends(users) так, чтобы она возвращала массив уникальных имён друзей (свойство friends) отсортированный по алфавиту .
+
+// Тесты
+// Объявлена переменная getSortedFriends
+// Переменной getSortedFriends присвоена стрелочная функция с параметром (users)
+// В теле функции используется цепочка методов в правильном порядке
+// Значение параметра users не изменяется
+// Вызов функции с указанным массивом пользователей возвращает массив ["Adrian Cross", "Aisha Tran", "Briana Decker", "Eddie Strong", "Goldie Gentry", "Jacklyn Lucas", "Jordan Sampson", "Linda Chapman", "Marilyn Mcintosh", "Naomi Buckner", "Padilla Garrison", "Sharron Pace", "Solomon Fokes"]
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
 
 // ============ SOLUTION 47 =============
 
+// const getSortedFriends = users =>
+//     users
+//         .flatMap(user => user.friends)
+//         .filter((friend, index, array) => array.indexOf(friend) === index)
+//         .sort((a, b) => a.localeCompare(b));
+
 // ============== TASK 48 ===============
 
+// Дополни функцию getTotalBalanceByGender(users, gender) так, чтобы она возвращала общий баланс пользователей (свойство balance), пол которых (свойство gender) совпадает со значением параметра gender.
+
+// Тесты
+// Объявлена переменная getTotalBalanceByGender
+// Переменной getTotalBalanceByGender присвоена стрелочная функция с параметрами (users, gender)
+// В теле функции используется цепочка методов в правильном порядке
+// Значение параметра users не изменяется
+// Если значение параметра gender это строка "male", функция возвращает число 12053
+// Если значение параметра gender это строка "female", функция возвращает число 8863
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
+
 // ============ SOLUTION 48 =============
+
+// const getTotalBalanceByGender = (users, gender) =>
+//     users
+//         .filter(user => user.gender === gender)
+//         .reduce((total, user) => {
+//             return total + user.balance;
+//         }, 0);
