f1f175a341661a618246c09ae8562065ea6b68db
MaryaSamsonenko <84477591+MaryaSamsonenko@users.noreply.github.com>
2021-09-24 18:57:26 +0300

    поправочка

--- js/autotest.js
+++ js/autotest.js
@@ -214,10 +214,20 @@
 //     return totalPrice;
 // }
 
+// function calculateTotalPrice(orderedItems) {
+//     let totalPrice = 0;
+//     // Change code below this line
+
+//     orderedItems.forEach((orderedItem, index) => (totalPrice += orderedItems[index]));
+
+//     // Change code above this line
+//     return totalPrice;
+// }
+
 // console.log(calculateTotalPrice([12, 85, 37, 4]));
 // console.log(calculateTotalPrice([164, 48, 291]));
 // console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
-// console.log();
+
 
 //  ======== Option 2
 
